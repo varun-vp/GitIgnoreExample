8c629879119a73f9e8894e9be7411e369fdd5cd9
varun-vp <varun.pahuja96@gmail.com>
2020-04-03 10:42:37 +0530

    first track with student controller and index file

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GitIgnoreConceptExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitIgnoreConceptExample.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> stlist = new List<Student>()
+            {
+                new Student{SId = 101, SName = "Sam", SMarks = 99},
+                new Student{SId = 102, SName = "Ram", SMarks = 90},
+                new Student{SId = 103, SName = "Shyam", SMarks = 99},
+            };
+            return View(stlist);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitIgnoreConceptExample.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public 
+            string SName { get; set; }
+        public float SMarks { get; set; }
+
+    }
+}
